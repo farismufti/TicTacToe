205d64d4c6146f28212975bf4afd7267190e0bbd
farismufti <faris.mufti@outlook.com>
2020-05-13 21:55:02 +0300

    Modified playMove method to allow for two players

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -44,43 +44,53 @@ public class TicTacToe {
     }
 
     public static void playMove(char[][] gameBoard, int position, String player) {
-        
+
+        char symbol = ' ';
+        if(player.equals("Player 1")) {
+
+            symbol = 'X';
+        }
+        else {
+
+            symbol = 'O';
+        }
+
         switch(position) {
 
             case 1:
-                gameBoard[0][0] = 'X';
+                gameBoard[0][0] = symbol;
                 break;
 
             case 2:
-                gameBoard[0][2] = 'X';
+                gameBoard[0][2] = symbol;
                 break;
 
             case 3:
-                gameBoard[0][4] = 'X';
+                gameBoard[0][4] = symbol;
                 break;
 
             case 4:
-                gameBoard[2][0] = 'X';
+                gameBoard[2][0] = symbol;
                 break;
 
             case 5:
-                gameBoard[2][2] = 'X';
+                gameBoard[2][2] = symbol;
                 break;
 
             case 6:
-                gameBoard[2][4] = 'X';
+                gameBoard[2][4] = symbol;
                 break;
 
             case 7:
-                gameBoard[4][0] = 'X';
+                gameBoard[4][0] = symbol;
                 break;
 
             case 8:
-                gameBoard[4][2] = 'X';
+                gameBoard[4][2] = symbol;
                 break;
 
             case 9:
-                gameBoard[4][4] = 'X';
+                gameBoard[4][4] = symbol;
                 break;
         }
     }
